c900866abf1e88936707192c97755ad0ec85ecbf
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-29 20:49:38 -0300

    Menu repetindo

--- src/prova2logica2020/problemaFuncionarios.java
+++ src/prova2logica2020/problemaFuncionarios.java
@@ -53,6 +53,27 @@ public class problemaFuncionarios {
 			}
 		}
 		System.out.println();
+
+		System.out.println("MENU");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
+		System.out.println("3 - Nome da pessoa que ganhou mais");
+		System.out.println("4 - Sair");
+		System.out.print("Digite uma opção: ");
+		int opcao = sc.nextInt();
+		System.out.println();
+		while (opcao != 4) {
+			System.out.println("MENU");
+			System.out.println("1 - Total de horas trabalhadas");
+			System.out.println("2 - Custo total");
+			System.out.println("3 - Nome da pessoa que ganhou mais");
+			System.out.println("4 - Sair");
+			System.out.print("Digite uma opção: ");
+			opcao = sc.nextInt();
+			System.out.println();
+		}
+		System.out.println("FIM DO PROGRAMA!");
+		sc.close();
 	}
 
 }
