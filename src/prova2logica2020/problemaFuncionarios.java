37960e351be6ea17731021ee616dc4f784bb0704
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-29 20:51:37 -0300

    Tratativas 1 e 2

--- src/prova2logica2020/problemaFuncionarios.java
+++ src/prova2logica2020/problemaFuncionarios.java
@@ -10,6 +10,10 @@ public class problemaFuncionarios {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		int totalHoras = 0;
+		double soma = 0.0;
+		double custoTotal = 0.0;
+
 		System.out.print("Nome: ");
 		String nome = sc.next();
 		System.out.print("Horas trabalhadas: ");
@@ -18,12 +22,16 @@ public class problemaFuncionarios {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			horasTrabalhadas = sc.nextInt();
 		}
+		totalHoras += horasTrabalhadas;
 		System.out.print("Valor por hora: ");
 		double valorHora = sc.nextDouble();
 		while (valorHora <= 0.0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			valorHora = sc.nextDouble();
 		}
+		soma = horasTrabalhadas * valorHora;
+		custoTotal += soma;
+
 		System.out.print("Digitar outro (S/N)? ");
 		String resp = sc.next();
 		while (resp.equals("S") == false && resp.equals("N") == false) {
@@ -39,12 +47,16 @@ public class problemaFuncionarios {
 				System.out.print("O valor deve ser positivo. Tente novamente: ");
 				horasTrabalhadas = sc.nextInt();
 			}
+			totalHoras += horasTrabalhadas;
 			System.out.print("Valor por hora: ");
 			valorHora = sc.nextDouble();
 			while (valorHora <= 0.0) {
 				System.out.print("O valor deve ser positivo. Tente novamente: ");
 				valorHora = sc.nextDouble();
 			}
+			soma = horasTrabalhadas * valorHora;
+			custoTotal += soma;
+
 			System.out.print("Digitar outro (S/N)? ");
 			resp = sc.next();
 			while (resp.equals("S") == false && resp.equals("N") == false) {
@@ -61,7 +73,17 @@ public class problemaFuncionarios {
 		System.out.println("4 - Sair");
 		System.out.print("Digite uma opção: ");
 		int opcao = sc.nextInt();
-		System.out.println();
+		if (opcao == 1) {
+			System.out.println();
+			System.out.println("Total de horas = " + totalHoras);
+			System.out.println();
+		} else if (opcao == 2) {
+			System.out.println();
+			System.out.printf("Custo total = R$ %.2f%n", custoTotal);
+			System.out.println();
+		} else {
+			System.out.println();
+		}
 		while (opcao != 4) {
 			System.out.println("MENU");
 			System.out.println("1 - Total de horas trabalhadas");
@@ -70,10 +92,20 @@ public class problemaFuncionarios {
 			System.out.println("4 - Sair");
 			System.out.print("Digite uma opção: ");
 			opcao = sc.nextInt();
-			System.out.println();
+
+			if (opcao == 1) {
+				System.out.println();
+				System.out.println("Total de horas = " + totalHoras);
+				System.out.println();
+			} else if (opcao == 2) {
+				System.out.println();
+				System.out.printf("Custo total = R$ %.2f%n", custoTotal);
+				System.out.println();
+			} else {
+				System.out.println();
+			}
 		}
 		System.out.println("FIM DO PROGRAMA!");
 		sc.close();
 	}
-
 }
