73a049c2a6849b2403937e10e47b70d188c6ea23
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-29 20:47:57 -0300

    Leitura dos dados

--- src/prova2logica2020/problemaFuncionarios.java
+++ src/prova2logica2020/problemaFuncionarios.java
@@ -10,7 +10,49 @@ public class problemaFuncionarios {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		
+		System.out.print("Nome: ");
+		String nome = sc.next();
+		System.out.print("Horas trabalhadas: ");
+		int horasTrabalhadas = sc.nextInt();
+		while (horasTrabalhadas <= 0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			horasTrabalhadas = sc.nextInt();
+		}
+		System.out.print("Valor por hora: ");
+		double valorHora = sc.nextDouble();
+		while (valorHora <= 0.0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			valorHora = sc.nextDouble();
+		}
+		System.out.print("Digitar outro (S/N)? ");
+		String resp = sc.next();
+		while (resp.equals("S") == false && resp.equals("N") == false) {
+			System.out.print("Digite S ou N Maiúsculo: ");
+			resp = sc.next();
+		}
+		while (resp.equals("N") == false) {
+			System.out.print("Nome: ");
+			nome = sc.next();
+			System.out.print("Horas trabalhadas: ");
+			horasTrabalhadas = sc.nextInt();
+			while (horasTrabalhadas <= 0) {
+				System.out.print("O valor deve ser positivo. Tente novamente: ");
+				horasTrabalhadas = sc.nextInt();
+			}
+			System.out.print("Valor por hora: ");
+			valorHora = sc.nextDouble();
+			while (valorHora <= 0.0) {
+				System.out.print("O valor deve ser positivo. Tente novamente: ");
+				valorHora = sc.nextDouble();
+			}
+			System.out.print("Digitar outro (S/N)? ");
+			resp = sc.next();
+			while (resp.equals("S") == false && resp.equals("N") == false) {
+				System.out.print("Digite S ou N Maiúsculo: ");
+				resp = sc.next();
+			}
+		}
+		System.out.println();
 	}
 
 }
